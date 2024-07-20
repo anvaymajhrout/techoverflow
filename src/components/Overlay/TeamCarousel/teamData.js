dbc917fe3e6e87c3a789971f372d33c8e3d6299a
anvaymajhrout <amajhrout@gmail.com>
2024-07-20 18:25:09 +0530

    testing

---
+++ src/components/Overlay/TeamCarousel/teamData.js
@@ -0,0 +1,64 @@
+// Team data
+const teamData = [
+  {
+    name: "Danney",
+    image: "/images/danney-min.jpg",
+    links: [
+      { title: "linkedin", link: "https://t.me/tech_overflow" },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+  {
+    name: "Sean",
+    image: "/images/sean-min.jpg",
+    links: [
+      { title: "linkedin", link: "https://t.me/tech_overflow" },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+  {
+    name: "Jane",
+    image: "/images/jane-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://t.me/tech_overflow",
+      },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+  {
+    name: "Szymon",
+    image: "/images/szymon-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://t.me/tech_overflow",
+      },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+  {
+    name: "Zoran",
+    image: "/images/zoran-min.jpg",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://t.me/tech_overflow",
+      },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+];
+
+export default teamData;
